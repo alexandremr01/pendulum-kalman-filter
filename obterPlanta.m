16ebbff41a1b30d09c4a47a886b66ca768dd74bc
vasconcelosdavi <davi.muniz41@gmail.com>
2021-07-09 12:05:40 -0300

    add pd controller

---
+++ obterPlanta.m
@@ -0,0 +1,6 @@
+function planta = obterPlanta()
+
+planta.m = 1;
+planta.l = 0.5;
+planta.g = 9.81;
+end
---
+++ obterRequisitos.m
@@ -0,0 +1,6 @@
+function requisitos = obterRequisitos()
+
+requisitos.wn = 1;
+requisitos.xi = 0.6;
+end
+
