00861ce5379198b821f2f386fa74e3175adf8f3b
alexandremr01 <alexandremr01@gmail.com>
2021-07-14 02:32:22 -0300

    add mean and std

--- executarTudo.m
+++ executarTudo.m
@@ -1,5 +1,5 @@
-thetar = 0;
-tf = 10;
+thetar = 0.6;
+tf = 100;
 varianciaComando = 10;
 varianciaMedida = 0.5;
 
@@ -8,5 +8,12 @@ requisitos = obterRequisitos();
 controlador = projetarControlador(requisitos, planta);
 model = obterParametros(planta, varianciaComando, varianciaMedida);
 
-sim_with_filter = simular(true, model, controlador, planta, thetar, tf, varianciaComando, varianciaMedida);
-sim_without_filter = simular(false, model, controlador, planta, thetar, tf, varianciaComando, varianciaMedida);
+simulacao_com_filtro = simular(true, model, controlador, planta, thetar, tf, varianciaComando, varianciaMedida);
+simulacao_sem_filtro = simular(false, model, controlador, planta, thetar, tf, varianciaComando, varianciaMedida);
+
+meio = floor(length(simulacao_sem_filtro.theta.signals.values) / 2);
+media_sem_filtro = mean(simulacao_sem_filtro.theta.signals.values(meio:end, 1));
+desv_sem_filtro = std(simulacao_sem_filtro.theta.signals.values(meio:end, 1));
+
+media_com_filtro = mean(simulacao_com_filtro.theta.signals.values(meio:end, 1));
+desv_com_filtro = std(simulacao_com_filtro.theta.signals.values(meio:end, 1));
